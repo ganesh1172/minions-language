693d45115a0e749095b3c64bec41ce19a0889970
ganesh1172 <ganesh172595@gmail.com>
2020-12-03 16:57:23 +0530

    call a mock server (from JS)

--- Script.js
+++ Script.js
@@ -2,7 +2,12 @@ var userClick = document.querySelector("#trans_btn");
 var txtInput = document.querySelector("#txt_input");
 var shwOpt = document.querySelector("#shw_opt");
 
+var serverApi = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
 function showOutput() {
+    fetch(serverApi)
+        .then(response => response.json())
+        .then(data => console.log(data));
     var userInput = txtInput.value;
     shwOpt.innerText = userInput;
     // console.log("Welcome " + userInput);
