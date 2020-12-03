427e4e8475911a060d8d7d25316d63871a9b8a1c
ganesh1172 <ganesh172595@gmail.com>
2020-12-03 16:42:49 +0530

    wire a div to show output

--- Script.js
+++ Script.js
@@ -1,9 +1,11 @@
 var userClick = document.querySelector("#trans_btn");
 var txtInput = document.querySelector("#txt_input");
+var shwOpt = document.querySelector("#shw_opt");
 
 function showOutput() {
     var userInput = txtInput.value;
-    console.log("Welcome " + userInput);
+    shwOpt.innerText = userInput;
+    // console.log("Welcome " + userInput);
 }
 
 userClick.addEventListener("click", showOutput);
