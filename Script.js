f324438af071660f3598f3bb20d3c8b14e69f380
ganesh1172 <ganesh172595@gmail.com>
2020-12-08 19:49:51 +0530

    wire fetch call in app and error handling

--- Script.js
+++ Script.js
@@ -1,16 +1,36 @@
 var userClick = document.querySelector("#trans_btn");
 var txtInput = document.querySelector("#txt_input");
 var shwOpt = document.querySelector("#shw_opt");
+var speak = document.querySelector("#read_opt");
 
-var serverApi = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverApi = "https://api.funtranslations.com/translate/minion.json";
+// var serverApi = "https://api.funtranslations.com/translate/groot.json";
+// var serverApi = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslatedUrl(text) {
+    return serverApi + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error occured" + error);
+    alert("caught error");
+}
 
 function showOutput() {
-    fetch(serverApi)
-        .then(response => response.json())
-        .then(data => console.log(data));
-    var userInput = txtInput.value;
-    shwOpt.innerText = userInput;
-    // console.log("Welcome " + userInput);
+    var inputTxt = txtInput.value;
+    fetch(getTranslatedUrl(inputTxt))
+        .then(res => res.json())
+        .then(data => {
+            var translatedTxt = data.contents.translated;
+            shwOpt.innerText = translatedTxt;
+        })
+        .catch(errorHandler);
+}
+
+function speakVoice() {
+    var speakVoice = shwOpt.innerText;
+    responsiveVoice.speak(speakVoice);
 }
 
 userClick.addEventListener("click", showOutput);
+speak.addEventListener("click", speakVoice);
